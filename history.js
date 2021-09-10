a93ed7d22a5fdd4d0f9b8a6191f14d7cbc49b8a5
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-10 19:41:54 +0600

    history api

---
+++ history.js
@@ -0,0 +1,6 @@
+// history.back  eta same tab er  ager page e niye jabe jetay ami cilam ektu age
+//history.forward eta shamner page e jabe same tab er
+
+//history.length eta dekhabe ami koyta page e gesi oi tab er 
+// history.go(0) eta same page ke refresh korbe.
+//history.go(-1) eta pichone nibe orthat eta history.back er moto kaaj kore . same vabe history.go(1) eta history.forward er moto kaaj kore.
