6e0731d57d8949932f633dde03a2fccc4c1d2757
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-11 17:00:17 +0600

    summery

---
+++ summery.js
@@ -0,0 +1,33 @@
+/*1. browser e API ache
+  2. document or DOM ke amra plain javascript e use korte parbona kintu browser e use korte parbo .
+  3.fetch keo browser e use korte parbo kintu plain js e use korte ba chalaite parbona.
+  4.alert('');  eta hosse browser e ekta pop up bar dekhabe and ami ja likhe dibo ta dekhabe alert hishebe.
+  5.confirm('are ypu single?'); eta dara bujhay user kichu ekta jiggesh korbe and eta ok or no diye ans nibe.
+  6.prompt('name'); eta mne hpsse input nite parbo user theke
+  7. http://127.0.0.1:5500/shopping.html
+ekhane
+  http or https hosse protocol ,
+  tar porer ongsho and colon er aage porjonto
+  host number ,
+  colon er por 5500 eta hosse port number ,
+  er baki ongsho hash 
+
+  //location.reload()
+  //location.href=''
+  //location.hash =
+  
+  //history.back()
+  //history.go()
+
+  //cookies => user er kichu information website e rakha jay cookies diye. cookies diye short time session cookie rakha jay abr long term er jonno persistent cookie o rakha jay.
+  //sessionStorage vs localStorage
+  //cookies vs sessionStorage vs localStorage
+
+  cookies gulo store hoy hosse semicolon diye separate hoye. R  oatke parse kore kora jay .
+  session storage and localstorage almost same . kintu session storage only session ta porjonto  thakbe . R localstorage hosse lomba shomoy porjonto store thakbe 
+
+
+  // shift+esc  dile browser e kon kon apps memory khasse koto ta show korbe
+
+
+  */
